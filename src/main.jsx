512cea28317db03aa8b3a0b0b5ef6cce12a05506
Lucas Vu <lucasvu26@gmail.com>
2025-09-13 23:17:20 -0400

    Fix HTML preload links and update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Cursor from "./components/ui/Cursor.jsx";
+import Home from "./components/HomePage.jsx";
+import Projects from "./components/Projects.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Cursor />
-    <App />
+    <HashRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </App>
+    </HashRouter>
   </React.StrictMode>
 );
